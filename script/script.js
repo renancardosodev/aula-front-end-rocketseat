ba2ca4088bbabf06c49a8f5c2c443dd6b4048d1b
Renan Cardoso <renancardoso.dev@gmail.com>
2023-06-07 18:03:05 -0300

    mudança de foto de perfil

--- script/script.js
+++ script/script.js
@@ -1,11 +1,14 @@
 function toggleMode(){
     const html = document.documentElement
+    html.classList.toggle('light')
 
-   // if(html.classList.contains('light')){
-   //     html.classList.remove('light')
-   // } else{
-   //     html.classList.add('light')
-   // }
-
-   html.classList.toggle('light')
+    const img = document.querySelector("header img")
+    if(html.classList.contains('light')){
+        img.setAttribute('src', '../assets/perfil-light.jpeg')
+        img.setAttribute('alt', 'imagem de perfil em preto e branco com fundo verde')
+    } else{
+        img.setAttribute('src', '../assets/perfil-dark.png')
+        img.setAttribute('alt', 'imagem de perfil com fundo azul escuro e amarelo')
+    }
+ 
 }
